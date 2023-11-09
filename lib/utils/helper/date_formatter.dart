cf78f45a1109e373ca019e6ed3298e26b88d599a
gaurav <imgauravchandani@gmail.com>
2023-11-09 13:00:51 +0530

    changes: relative day names with current date

--- lib/utils/helper/date_formatter.dart
+++ lib/utils/helper/date_formatter.dart
@@ -1,5 +1,18 @@
 import 'package:employee_management_app/core/constants/constants.dart';
 
 String formatDate(DateTime date) {
-  return "${date.day} ${monthNames[date.month-1].substring(0, 3)} ${date.year}";
+  var now = DateTime.now();
+  var today = DateTime(now.year, now.month, now.day);
+  var dayOfDate = DateTime(date.year, date.month, date.day);
+  var diff = dayOfDate.difference(today).inDays;
+  if (diff <= 1 && diff >= -1) {
+    if (diff == 1) {
+      return "Tomorrow";
+    } else if (diff == 0) {
+      return "Today";
+    } else {
+      return "Yesterday";
+    }
+  }
+  return "${date.day} ${monthNames[date.month - 1].substring(0, 3)} ${date.year}";
 }
